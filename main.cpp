8dfec0baf1b44f9273bbbc2a4aeaf3470c07859b
Maitreya L. <maitu.clever@gmail.com>
2024-11-19 14:12:44 +0100

    Test commit branch

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main(){
 
     std::cout<<"Hello World"<<std::endl;
 
-    return 0;
+}
 
+void Test1()
+{
+    std::cout<<"This is a test. It won't print out though. It's a void after all."<<std::endl;
 }
