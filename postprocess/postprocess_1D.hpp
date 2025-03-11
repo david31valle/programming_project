1843066449b9545508b7e144d9ccdb233fecf648
Maitreya L. <maitu.clever@gmail.com>
2025-03-11 18:54:00 +0100

    Plots for 1D and 2D. 3D has errors

---
+++ postprocess/postprocess_1D.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Maitreya Limkar on 10-03-2025.
+//
+
+#ifndef PREPROCESS_1D_HPP
+#define PREPROCESS_1D_HPP
+
+
+
+#endif //PREPROCESS_1D_HPP
---
+++ postprocess/postprocess_2D.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Maitreya Limkar on 11-03-2025.
+//
+
+#ifndef POSTPROCESS_2D_HPP
+#define POSTPROCESS_2D_HPP
+
+#endif //POSTPROCESS_2D_HPP
